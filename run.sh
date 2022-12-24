1304fa5b0ad7c7137f0a550f536ec548e751f838
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-12-24 14:13:35 -0300

    Add bash script to automatically install virtual environment, if it does not exist

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 gcc ./trees/main.c ./trees/avl-tree/avltree-counter.c ./trees/b-tree/btree-counter.c ./trees/red-black-tree/redblacktree-counter.c -o ./trees/main.o
 
 ./trees/main.o
 
+if ! [ -d ./image-generator/venv ]; then
+  echo "No virtual environment found. Installing packages..."
+  python -m venv ./image-generator/venv
+  source ./image-generator/venv/bin/activate
+  pip install -r ./image-generator/requirements.txt
+else
+  echo "Virtual environment found. Skipping installation of Python packages."
+fi
+
+
 ./image-generator/venv/bin/python ./image-generator/generateScatteredPoints.py
 ./image-generator/venv/bin/python ./image-generator/generateRegression.py
 
+echo "Files generated! Check ./files directory."
 
